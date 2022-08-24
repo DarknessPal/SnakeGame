422e6cf92a151639564f1e7501683a355b54b8c0
DarknessPal <95918963+DarknessPal@users.noreply.github.com>
2022-08-24 12:56:12 +0300

    Update Apple.java
    deleted draw method

--- SnakeGame/src/snake/Apple.java
+++ SnakeGame/src/snake/Apple.java
@@ -4,8 +4,4 @@ public class Apple extends GameObject{
     Apple (int x, int y){
         super (x, y);
     }
-    private final static String APPLE_SIGN = "\uD83C\uDF4E";
-    public void draw(Game game){
-//        game.setCellValue();
-    }
 }

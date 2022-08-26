8a90abb6af10ac7a98ae270490e1369426cee8af
DarknessPal <nightknight11@gmail.com>
2022-08-26 23:57:53 +0300

    Fix bug with appearance a new apple. Fix bug with multitouch. Add pause and game over. Fix appearance a new apple under the snake.

--- SnakeGame/src/snake/SnakeGame.java
+++ SnakeGame/src/snake/SnakeGame.java
@@ -10,9 +10,12 @@ import java.util.Objects;
 import java.util.Random;
 
 public class SnakeGame extends JPanel implements ActionListener{
+    private boolean isPause = false;  // Pause
+    private boolean isFail = false;    // Gameover
 
     Timer timer = new Timer(150, this);
     Random random;
+    private String snakeKeyMove = "R";
     private final int width = 500;
     private final int height = 500;
     private final int unitSize = 20;
@@ -33,13 +36,23 @@ public class SnakeGame extends JPanel implements ActionListener{
         this.setFocusable(true);
         this.addKeyListener(new MyKey());
         random = new Random();
- //       timer = new Timer(100, this);
         timer.start();
     }
 
     public void paintComponent(Graphics graphic) {
         super.paintComponent(graphic);
         draw(graphic);
+        if (isPause) {
+            graphic.setColor(Color.BLACK);
+            graphic.setFont(new Font("Microsoft Yahei", Font.BOLD, unitSize));
+            graphic.drawString("Press space to continue the game", width/unitSize, height/2);
+        }
+        if (isFail) {
+            graphic.setColor(Color.RED);
+            graphic.setFont(new Font("Microsoft Yahei", Font.BOLD, unitSize));
+            graphic.drawString("Gameover", width/2, height/2);
+        }
+        repaint();
     }
     public void draw(Graphics graphic) {
         graphic.setColor(Color.GREEN);
@@ -52,70 +65,88 @@ public class SnakeGame extends JPanel implements ActionListener{
     
     @Override
     public void actionPerformed(ActionEvent arg0) {
-        if(snake.x[0]== apple.x && snake.y[0]==apple.y){  //eat Apple
-            snake.length++;
-            apple.x = random.nextInt((width)/unitSize)*unitSize;
-            apple.x = random.nextInt((height)/unitSize)*unitSize;
-        }
-        for (int i = snake.length - 1; i > 0; i--) {  //body move
-            snake.x[i] = snake.x[i-1];
-            snake.y[i] = snake.y[i-1];
-            }
-        if(snake.move.equals("R")){  // move right
-            snake.x[0] += unitSize;
-        }
-        if(snake.move.equals("U")){  // move up
-            snake.y[0] += unitSize;
-        }
-        if(snake.move.equals("L")){  // move left
-            snake.x[0] -= unitSize;
-        }
-        if(snake.move.equals("D")){  // move down
-            snake.y[0] -= unitSize;
-        }
-            if (snake.x[0] == width) {
-                snake.x[0] = 0;
-            }
-            if (snake.x[0] == (-unitSize)) {
-                snake.x[0] = (width-unitSize);
-            }
-            if (snake.y[0] == height) {
-                snake.y[0] = 0;
-            }
-            if (snake.y[0] == (-unitSize)) {
-                snake.y[0] = (height-unitSize);
-            }
+        if (!isFail) {
+            if (!isPause) {
+                if (snake.length >4) {
+                    for (int i = 1; i < snake.length - 1; i++) {
+                        if (snake.x[0] == snake.x[i] && snake.y[0] == snake.y[i]) {
+                            isFail = !isFail;
+                        }
+                    }
+                }
+                if (snake.x[0] == apple.x && snake.y[0] == apple.y) {  //eat Apple
+                    snake.length++;
+                    apple.x = random.nextInt((width) / unitSize) * unitSize;
+                    apple.y = random.nextInt((height) / unitSize) * unitSize;
+                    START: for (int i = 0; i < snake.length; i++) {
+                        if(apple.x == snake.x[i] && apple.y == snake.y[i]){
+                            apple.x = random.nextInt((width) / unitSize) * unitSize;
+                            apple.y = random.nextInt((height) / unitSize) * unitSize;
+                            continue START;
+                        }
+                    }
+                }
+                for (int i = snake.length - 1; i > 0; i--) {  //body move
+                    snake.x[i] = snake.x[i - 1];
+                    snake.y[i] = snake.y[i - 1];
+                }
+                if (snake.move.equals("R")) {  // move right
+                    snake.x[0] += unitSize;
+                }
+                if (snake.move.equals("U")) {  // move up
+                    snake.y[0] += unitSize;
+                }
+                if (snake.move.equals("L")) {  // move left
+                    snake.x[0] -= unitSize;
+                }
+                if (snake.move.equals("D")) {  // move down
+                    snake.y[0] -= unitSize;
+                }
+                if (snake.x[0] == width) {
+                    snake.x[0] = 0;
+                }
+                if (snake.x[0] == (-unitSize)) {
+                    snake.x[0] = (width - unitSize);
+                }
+                if (snake.y[0] == height) {
+                    snake.y[0] = 0;
+                }
+                if (snake.y[0] == (-unitSize)) {
+                    snake.y[0] = (height - unitSize);
+                }
 //        System.out.println("snake.x[0] in position "  + " = " + snake.x[0]);
 //        System.out.println("snake.y[0] in position "  + " = " + snake.y[0]);
-        timer.start();
-        repaint();
+                repaint();
+                snake.move = snakeKeyMove;
+            }
+        }
     }
-
-
     public class MyKey extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e) {
             //key logic
             int keyCode = e.getKeyCode();
-            if (keyCode == 32){}  // space
+            if (keyCode == 32){  // space
+                isPause = !isPause;
+            }
             if (keyCode == 39 ||keyCode == 68){   // key right
                 if(!Objects.equals(snake.move, "L")) {
-                    snake.move = "R";
+                    snakeKeyMove = "R";
                 }
             }
             if (keyCode == 40 ||keyCode == 83){   // key up
                 if(!Objects.equals(snake.move, "D")) {
-                    snake.move = "U";
+                    snakeKeyMove = "U";
                 }
             }
             if (keyCode == 37 ||keyCode == 65){   // key left
                 if(!Objects.equals(snake.move,"R")) {
-                    snake.move = "L";
+                    snakeKeyMove = "L";
                 }
             }
             if (keyCode == 38 ||keyCode == 87){    //  key down
                 if(!Objects.equals(snake.move,"U")) {
-                    snake.move = "D";
+                    snakeKeyMove = "D";
                 }
             }
         }

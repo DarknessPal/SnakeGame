5055c9bc7afbc6e3701ee44760a2613ec54c378d
DarknessPal <nightknight11@gmail.com>
2022-08-30 19:06:30 +0300

    .

--- SnakeGame/src/snake/MainMenu.java
+++ SnakeGame/src/snake/MainMenu.java
@@ -3,80 +3,49 @@ package snake;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.*;
 
-public class MainMenu extends JFrame implements ActionListener {
+public class MainMenu extends JPanel implements ActionListener {
 
-//    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+
     public MainMenu() {
-        JFrame frame = new JFrame("Game menu");
-        final int width = 400;
-        final int height = 400;
-        this.setPreferredSize(new Dimension(width, height));
-        this.setBackground(Color.white);
-        this.setFocusable(true);
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
         //    this.addKeyListener(new MainMenu.MyKey());
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null);
-        this.setResizable(false);
-        this.pack();
-        this.setVisible(true);
-
         // set flow layout for the frame
-        this.getContentPane().setLayout(new FlowLayout());
-
         JButton startGameButton = new JButton("Star the new game");
-
+        JButton settingGameButton = new JButton("Settings");
+        JButton exit = new JButton("Exit");
         //add Button
         add(startGameButton);
-
+        add(settingGameButton);
+        add(exit);
         //set action listeners for buttons
         startGameButton.addActionListener(this);
-
+        settingGameButton.addActionListener(this);
+        exit.addActionListener(this);
         // define a custom short action command for the button
         startGameButton.setActionCommand("Start the new game");
-
-        // add button to frame
-        add(startGameButton);
-
+        setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
         String action = ae.getActionCommand();
         if (action.equals("Start the new game")) {
-            new SnakeGame();
-            this.setTitle("Snake");
-        }
-    }
+            Screen.set = 1;
 
+        }
+        if (action.equals("Settings")) {
+            Screen.set = 2;
+        }
+        if (action.equals("Exit")) {
+            System.exit(0);
+        }
+
+    }
     private static void createAndShowGUI() {
 
-        //Create and set up the window.
-
-  /*      JFrame frame = new MainMenu();
-
-        //Display the window.
-
-        frame.pack();
-
-        frame.setVisible(true);
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);*/
-
     }
-
-  /*  public static void main(String[] args) {
-
-        //Schedule a job for the event-dispatching thread:
-
-        //creating and showing this application's GUI.
-
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }*/
 }
--- SnakeGame/src/snake/Screen.java
+++ SnakeGame/src/snake/Screen.java
@@ -1,15 +1,32 @@
 package snake;
 import javax.swing.*;
+import java.awt.*;
 
 
 public class Screen extends JFrame{
+    public static int set = 0;
 
     public Screen(){
-        new MainMenu();
-    /*    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final int width = 400;
+        final int height = 400;
+        this.setPreferredSize(new Dimension(width, height));
+        this.setLayout(null);
+        this.setBackground(Color.white);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
+        this.setLocationRelativeTo(null);
+        if (set == 0){
+            add(new MainMenu());
+            setTitle("Main Menu");
+        }
+        if (set == 1){
+            this.add(new SnakeGame());
+            this.setTitle("Snake");
+        }
+        if (set == 2){
+            this.setTitle("Settings");
+        }
         this.setVisible(true);
-        this.setLocationRelativeTo(null);*/
     }
 }
